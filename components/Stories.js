9123f7d771ac7b3c63aa0d7da96bbb5b69348016
ZikXewen <datasup12@gmail.com>
2021-10-29 19:17:42 +0700

    session basic

--- components/Stories.js
+++ components/Stories.js
@@ -1,7 +1,9 @@
 import faker from "faker";
 import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import Story from "./Story";
 export default () => {
+  const { data: session } = useSession();
   const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
     setSuggestions(
@@ -13,6 +15,9 @@ export default () => {
   }, []);
   return (
     <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story img={session.user.image} username={session.user.username} />
+      )}
       {suggestions.map((profile) => (
         <Story
           key={profile.id}
